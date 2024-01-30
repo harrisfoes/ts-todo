9fd6db9989bed10a89f88d1a7588b4cbf30032d0
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2024-01-30 20:02:20 +0900

    todo list app ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,103 @@
 import "./index.css";
-import {useState} from "react";
+import { useState } from "react";
 import Button from "./assets/Button";
 
 type TodoItem = {
-  id: number;
+  id: string;
   text: string;
-  completed:boolean;
-}
+  completed: boolean;
+};
 
 const App = () => {
-  const [todoList, setTodoList] = useState()
-  const [input, setInput] = useState<TodoItem[]>([])
+  const [todoList, setTodoList] = useState([]);
+  const [input, setInput] = useState<TodoItem[]>([]);
 
-  const handleInput = (event: React.ChangeEvent<HTMLInputElement>):void => {
-    setInput(event.target.value)
-  }
+  const handleInput = (event): void => {
+    setInput(event.target.value);
+  };
 
-  const handleOnClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log(event);
-    console.log("pinota")
-    
-  }
+  const handleOnClick = (event) => {
+    console.log(event.target);
+    console.log("pinota");
+    console.log(input);
+    addTodoItem(input);
+  };
 
-  const addTodoItem = (text:string) => {
+  const addTodoItem = (text: string) => {
     const newTodo: TodoItem = {
-      id: todoList.length + 1,
+      id: text + "_" + todoList.length,
       text,
-      completed:false,
+      completed: false,
     };
 
-    setTodoList([...todoList, newTodo])
-  }
+    setTodoList([...todoList, newTodo]);
+  };
 
-  console.log(todoList)
+  const handleComplete = (id: string) => {
+    console.log(id, "this is the id");
+    const newList = todoList.map((items) => {
+      if (id === items.id) return { ...items, completed: !items.completed };
+      else return items;
+    });
+
+    console.log(newList);
+    setTodoList(newList);
+  };
+
+  const handleRemove = (id: string) => {
+    const newList = todoList.filter((items) => {
+      if (id !== items.id) {
+        return items;
+      }
+    });
+
+    console.log(newList);
+
+    setTodoList(newList);
+  };
+
+  console.log(todoList);
 
   return (
-    
-
     <main className="container flex justify-center items-center bg-slate-000 mx-auto">
-    <div className="p-6 border shadow-xl bg-neutral-100 py-12 mt-12 rounded-lg">
-      <h1 className="text-3xl font-bold text-stone-700">Hello This is A Todo List App</h1>
-      <div className="mx-auto my-8 flex gap-4 items-center justify-center w-full">
-      <input value={input} onChange={handleInput} className="px-2 py-4 rounded-lg w-8/12 focus:neutral-200" type="text" name="searchbar" placeholder="Add Task" />
-      <Button content={"Add Task"} handleOnClick={handleOnClick}/>
+      <div className="p-6 border shadow-xl bg-neutral-100 py-12 mt-12 rounded-lg w-11/12 flex flex-col items-center">
+        <h1 className="text-3xl font-bold text-stone-700">
+          Hello This is A Todo List App
+        </h1>
+        <div className="mx-auto my-8 flex gap-4 items-center justify-center w-full">
+          <input
+            value={input}
+            onChange={handleInput}
+            className="px-2 py-4 rounded-lg w-8/12 focus:neutral-200"
+            type="text"
+            name="searchbar"
+            placeholder="Add Task"
+          />
+          <Button content={"Add Task"} handleOnClick={handleOnClick} />
+        </div>
+        <ol className="rounded-lg bg-slate-100 flex p-4 flex-col justify-center w-2/3 mx-auto">
+          {todoList.map((items, index) => (
+            <li
+              key={items.text + index}
+              className=" bg-slate-100 px-2 py-2 flex items-center justify-between"
+            >
+              <input
+                type="checkbox"
+                name={items}
+                checked={items.completed}
+                onChange={() => handleComplete(items.id)}
+              />
+              {items.text}{" "}
+              <button
+                onClick={() => handleRemove(items.id)}
+                className="shadow-lg hover:opacity-75 bg-slate-500 font-bold text-sky-100 px-4 py-2 rounded-lg"
+              >
+                Delete Task
+              </button>
+            </li>
+          ))}
+        </ol>
       </div>
-    </div>
     </main>
   );
 };
--- src/assets/Button.tsx
+++ src/assets/Button.tsx
@@ -1,17 +1,19 @@
-
 type ButtonProps = {
   style?: React.CSSProperties;
-  content: string
-  handleOnClick: () => void
-}
+  content: string;
+  handleOnClick: () => void;
+};
 
-function Button({
-  style,
-  content,
-  handleOnClick
-}: ButtonProps) {
-  
-  return <button style={style} onClick={handleOnClick} className="bg-slate-700 text-white px-2 py-2 rounded-lg hover:opacity-50">{content}</button>;
+function Button({ style, content, handleOnClick }: ButtonProps) {
+  return (
+    <button
+      style={style}
+      onClick={handleOnClick}
+      className="bg-slate-700 text-slate-100 px-2 py-2 rounded-lg hover:opacity-50 font-bold"
+    >
+      {content}
+    </button>
+  );
 }
 
 export default Button;
